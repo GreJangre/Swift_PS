b6dfb9005a06edef0fc8e930f371925d7eb13f15
GreJangre <dlatjdwn8824@naver.com>
2019-04-25 20:57:36 +0900

    657. Robot Return to Origin 메모리 최적화

--- Swift_PS/Robot_Return_to_Origin.playground/Contents.swift
+++ Swift_PS/Robot_Return_to_Origin.playground/Contents.swift
@@ -3,37 +3,27 @@
  
  이동 순서는 문자열로 표시되고 문자 moves[i]은 i 번째 이동을 나타냅니다. 유효한 이동은 R (오른쪽), L (왼쪽), U (위) 및 D (아래)입니다. 로봇이 모든 이동을 완료 한 후 원점으로 돌아 오면 true를 반환합니다. 그렇지 않은 경우는 false를 리턴합니다.
  
- 참고 : 로봇이 "마주 보는"방법은 부적합합니다. "R"은 항상 로봇을 오른쪽으로 한 번 움직이게 할 것이고, "L"은 항상 왼쪽으로 움직이게 할 것입니다. 또한, 로봇 움직임의 크기는 각 움직임마다 동일하다고 가정하십시오.
+ 참고 : 로봇이 "마주 보는" 방법은 부적합합니다. "R"은 항상 로봇을 오른쪽으로 한 번 움직이게 할 것이고, "L"은 항상 왼쪽으로 움직이게 할 것입니다. 또한, 로봇 움직임의 크기는 각 움직임마다 동일하다고 가정하십시오.
  */
 
 func judgeCircle(_ moves: String) -> Bool {
     
-    var location: Int = 0
+    var locationHorizen: Int = 0
+    var locationVertical: Int = 0
     
     for move in moves {
-        switch move {
-        case "R":
-            location += 1
-            break
-        case "L":
-            location -= 1
-            break
-        case "U":
-            location += 2
-            break
-        case "D":
-            location -= 2
-        default:
-            location = 0
-            break
+        if move == "R" {
+            locationHorizen += 1
+        } else if move == "L" {
+            locationHorizen -= 1
+        } else if move == "U" {
+            locationVertical += 1
+        } else {
+            locationVertical -= 1
         }
     }
     
-    if location == 0 {
-        return true
-    } else {
-        return false
-    }
+    return locationHorizen == 0 && locationVertical == 0
 }
 
 judgeCircle("UD")
