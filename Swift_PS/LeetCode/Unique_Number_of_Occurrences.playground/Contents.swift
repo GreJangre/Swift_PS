9cf1c516a96d3286782019c41ac25f9fe964e5c0
GreJangre <dlatjdwn8824@naver.com>
2019-10-02 11:00:06 +0900

    1207. Unique Number of Occurrences

--- Swift_PS/LeetCode/Unique_Number_of_Occurrences.playground/Contents.swift
+++ Swift_PS/LeetCode/Unique_Number_of_Occurrences.playground/Contents.swift
@@ -2,28 +2,49 @@
  정수 arr의 배열이 주어지면 배열의 각 값 발생 수가 고유한 경우에만 true를 리턴하는 함수를 작성하십시오.
  */
 
-func uniqueOccurrences(_ arr: [Int]) -> Bool {
+func uniqueOccurrencess(_ arr: [Int]) -> Bool {
     var subArray = arr
     var count = arr.count
     var chkCount = count
     var boolArray = Array(repeating: false, count: count)
     
     for i in arr {
+        subArray.removeAll() { $0 == i }
+        count = subArray.count
+        
         if chkCount > count {
             if boolArray[chkCount - count] == true {
                 return false
             }
             boolArray[chkCount - count] = true
-            //print(boolArray)
             chkCount = count
         }
-        subArray.removeAll() { $0 == i }
-        count = subArray.count
-        //print(count)
     }
     return true
 }
 
-//uniqueOccurrences([-3,0,1,-3,1,1,1,-3,10,0])
+func uniqueOccurrences(_ arr: [Int]) -> Bool {
+    var subArray: [Int: Int] = [:]
+    for i in arr {
+        if subArray[i] != nil {
+            subArray[i]! += 1
+        } else {
+            subArray[i] = 1
+        }
+    }
+    
+    var subArray2: [Int: Bool] = [:]
+    for value in subArray.values {
+        if subArray2[value] != nil {
+            return false
+        }
+        subArray2[value] = true
+    }
+    
+    return true
+}
+
+uniqueOccurrences([-3,0,1,-3,1,1,1,-3,10,0])
 uniqueOccurrences([1,2])
-//uniqueOccurrences([1,2,2,1,1,3])
+uniqueOccurrences([1,2,2,1,1,3])
+uniqueOccurrences([26,2,16,16,5,5,26,2,5,20,20,5,2,20,2,2,20,2,16,20,16,17,16,2,16,20,26,16])
